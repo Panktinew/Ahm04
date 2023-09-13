dbb968c9731aee9055a499c92453923e0b97aa5e
Panktinew <130337976+Panktinew@users.noreply.github.com>
2023-09-13 19:35:06 +0530

    Add files via upload

---
+++ Day1_Intro/src/First_Class.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  int  evenno ;
+	  int oddno;
+	  
+	  
+	  
+	  Scanner s1 = new Scanner(System.in);
+	  
+	  System.out.println("Enter number");
+	  evenno = s1.nextInt();
+	  System.out.println("Enter number");
+	  oddno = s1.nextInt();
+		
+       		   
+	
+	
+		
+	
+		System.out.println("hello!! Welcome to the platform" + evenno + " " + oddno);
+
+	}
+
+}
